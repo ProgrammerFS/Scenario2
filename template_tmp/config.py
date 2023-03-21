cb4318cb72cfc37a6c00ecefeed03059e7c700c9
Andy_Wu <89336613+Andy-Wu25@users.noreply.github.com>
2023-03-21 18:04:05 +0000

    email using flask

---
+++ template_tmp/config.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_mail import Mail
+from mailbox import Message
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv('/Users/andywu/Documents/Uni_programming/Scenario_2/test/.env')
+
+app = Flask(__name__)
+app.config['MAIL_SERVER']=os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.getenv('Scenario2group25@gmail.com')
+app.config['MAIL_PASSWORD'] = os.getenv('zwco gwie ntlg jivy')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
